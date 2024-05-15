298d7de8a1eaf551afef1cb5449eeca13ef272e8
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-15 18:54:37 +0530

    socket.io & integreated some comment

--- index.js
+++ index.js
@@ -4,20 +4,37 @@ const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
+// Socket server created
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Socket.io
+// 
 io.on("connection", (socket) => {
+  // Print the unique socket id
+  console.log(socket.id);
+
+  // user-message is same as client side
   socket.on("user-message", (message) => {
     io.emit("message", message);
-  });
-});
+    // Print the message sended by the client
+    console.log("user message", message);
+  })
 
-app.use(express.static(path.resolve("./public")));
+  // Print the message when socket is diconnected
+  socket.on("disconnect", () => {
+    console.log("User disconnect");
+  })
+})
 
+// File path resolved
+app.use(express.static(path.resolve("./Public")))
+
+// File rendering
 app.get("/", (req, res) => {
-  return res.sendFile("/public/index.html");
-});
+  return res.sendFile("/Public/index.html");
+})
 
-server.listen(9000, () => console.log(`Server Started at PORT:9000`));
+// The port is associated to the io server
+server.listen(9000, (port) => {
+  console.log("App is live");
+})
