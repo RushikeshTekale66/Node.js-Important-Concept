231af3cfa9312d3c56d5c8e33373b1577c095ae7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-15 11:01:27 +0530

    socket.io package

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static(path.resolve("./Public")));
-
-app.get("/", (req, res)=>{
-    return res.sendFile("/Public/index.html");
-})
-
-app.listen(9000, ()=>{
-    console.log("Server Started");
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
 });
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
