98230fe77825604675c8ceb2c0b98d4dfc7ab714
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-22 11:10:32 +0530

    mongoose : Query find() with query

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const User = require('./db');
 
 const findData = async()=>{
-  let result = await User.findOne({});
+  let result = await User.findOne({name:"Rushikesh Tekale"});
   console.log(result);
 }
 
 const findManyData = async()=>{
-  let result = await User.find({});
+  let result = await User.find({rollno : {$gt:2}});
   console.log(result);
 }
 
