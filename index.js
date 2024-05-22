07bc82ec64fa2f4d290373d867bb8035700d838a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-22 10:45:54 +0530

    mongoose : findData find(), findOne()

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
 const User = require('./db');
 
-// Insert single data in database
-const insertData = async () => {
-  const user = new User({
-    name: "Vaibhav Tambare",
-    rollno: "1"
-  })
-
-  try{
-    const saveUser = await user.save();
-    console.log("User saved", saveUser);
-  }
-  catch(e){
-    console.log("Error on saving", e);
-  }
+const findData = async()=>{
+  let result = await User.findOne({});
+  console.log(result);
 }
 
-// Insert multiple data in database
-
-const insertManyData = async()=>{
-  const users = [
-    {
-      name:"Adinath Tekale",
-      rollno:2
-    },
-    {
-      name:"Rushikesh Tekale",
-      rollno:3
-    }
-  ]
-
-  try{
-    const saveUsers = await User.insertMany(users);
-    console.log("Saved All user", saveUsers);
-  }
-  catch(e){
-    console.log("Error while saving data ", e);
-  }
+const findManyData = async()=>{
+  let result = await User.find({});
+  console.log(result);
 }
 
-// Insert One Data in database 
-insertData();
-insertManyData();
+findData();
+findManyData();
