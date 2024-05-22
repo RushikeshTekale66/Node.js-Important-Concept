95a681d39e5127cc0dbc9968424d988ec0ac9334
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-22 09:35:28 +0530

    mongoose : create db, create collection

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-const http = require("http");
-const express = require("express");
-const path = require("path");
-const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+// Database URL
+const url = "mongodb://127.0.0.1:27017/PracticeMongoDB";
 
-const app = express();
-// Socket server created
-const server = http.createServer(app);
-const io = new Server(server);
+// Connect Node.js to database
+mongoose.connect(url).then(()=>console.log("Connected to database")).catch((e)=>console.log("Error occure",e))
 
-// 
-io.on("connection", (socket) => {
-  // Print the unique socket id
-  console.log(socket.id);
-
-  // user-message is same as client side
-  socket.on("user-message", (message) => {
-    io.emit("message", message);
-    // Print the message sended by the client
-    console.log("user message", message);
-  })
-
-  // Print the message when socket is diconnected
-  socket.on("disconnect", () => {
-    console.log("User disconnect");
-  })
+const newSchema = new mongoose.Schema({
+  name:String,
+  rollno:Number
 })
 
-// File path resolved
-app.use(express.static(path.resolve("./Public")))
-
-// File rendering
-app.get("/", (req, res) => {
-  return res.sendFile("/Public/index.html");
-})
-
-// The port is associated to the io server
-server.listen(9000, (port) => {
-  console.log("App is live");
-})
+// create collection named infos
+const newModel = mongoose.model("info", newSchema);
