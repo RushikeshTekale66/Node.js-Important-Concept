3258913bec2e2d49e27d476ba7d7abfe3f8fdde6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-24 17:44:07 +0530

    Node: Modules - User Defined Modules

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi Rushikesh")
+// Single Module
+const SingleModule = require("./SingleExport");
+console.log(SingleModule(1,4));
+
+// Multiple Mudule exports
+const MultipleModule = require("./MultipleExports");
+console.log(MultipleModule.add(1,2));
+console.log(MultipleModule.sub(5,4));
+
+// exports object
+const ExportsObject = require("./ExportsObject");
+console.log(ExportsObject.add(5,5));
+console.log(ExportsObject.sub(10,5));
