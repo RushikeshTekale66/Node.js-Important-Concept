241107b855a61b14bc551675971e87b157443a07
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-24 17:04:36 +0530

    Node : hellow world

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const mongoose = require('mongoose');
-
-// db can access the database which is connected
-const db = mongoose.connection;
-
-const dropCollectionDoc = async()=>{
-  try{
-    // drop the collection "infos"
-    await db.dropCollection("infos");
-    console.log("Collection deleted successfully");
-  }
-  catch(e){
-    console.log("Error on droping ", e);
-  }
-}
-
-dropCollectionDoc();
+console.log("Hi Rushikesh")
