26fc83e0480218bed4d4de9947b1726221861c71
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-27 23:30:08 +0530

    Node : Rest Api http-methods get, post, patch, delete

--- index.js
+++ index.js
@@ -19,11 +19,28 @@ app.get(('/api/users'), (req, res) => {
   return res.json(users);
 })
 
-// Get data as per id which is provided
+// Get data as per id which is provided ()
 app.get("/api/users/:id", (req, res)=>{
   const id = Number(req.params.id);
   const user = users.find((user)=>user.id===id);
   res.json(user);
+})
+
+// send data to database by post method
+app.post("/api/user", (req, res)=>{
+  // create new user
+  return res.json({status:"pending"});
+})
+
+// Update data
+app.patch("/api/users/:id", (req, res)=>{
+  // Update user with id
+  return res.json({status:"pending"});
+})
+
+app.delete("/api/users", (req, res)=>{
+  // Delete user with id
+  return res.json({status:"pending"});
 })
 
 app.listen(port, () => { console.log(`App is live on ${port}`); })
