4b45d178330903d3a9d84c8f2e8487d63b2ec4b6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-28 10:25:09 +0530

    Node: API headers - http headers

--- index.js
+++ index.js
@@ -3,24 +3,10 @@ const User = require("./MOCK_DATA.json")
 
 const app = express();
 
-// Middle where 1
-app.use((req, res, next)=>{
-  // we also change the value of request
-  req.name = "Rushikesh";
-  console.log("Middle where 1", req.name);
-  // next call the next function, if not next then result drop here
-  next();
-})
-
-app.use((req, res, next)=>{
-  // we access the request value anywhere
-  console.log("Middle where 2", req.name);
-  next()
-})
-
 app.get("/getuser", (req, res)=>{
+  // setting header
+  res.setHeader("myName", "Rushikeshtekale")
   let result = User;
-  console.log(req.name);
   res.json(result);
 })
 
