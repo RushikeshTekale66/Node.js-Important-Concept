c1e196fd2a8d23ceadd934aadd463ab78c6fda19
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-26 12:50:56 +0530

    Route level middleware

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express");
+const middle = require('./Middle');
 const app = express();
+const router = express.Router();
 const port = 5000;
 
-//Middle ware
-const middle = (req, res, next)=>{
-    // If route contains login the go further
-    if(req.query.login){
-        console.log(req.query);
-        next();
-    }
-    //else print error
-    else{
-        res.send("<h1>You are not login</h1>");
-    }    
-}
-
-//Middleware using in app
-app.use(middle);
-
-
-//route level middleware
+//Direct middle ware
 app.get("/", middle, (req, res)=>{
     res.send("<h1>Hi I am Home page");
 })
 
+//middleware for multiple route
+router.use(middle);
+
+router.get("/about", (req, res)=>{
+    res.send("<h1>I am About page</h1>")
+})
+
+router.get("/contact", (req, res)=>{
+    res.send("<h1>I am contact page</h1>")
+})
+
+app.get("/profile", (req, res)=>{
+    res.send("<h1>I am profile page</h1>");
+})
+
+//Route level Middleware
+app.use("/", router);
 app.listen(port, ()=>{
     console.log("Application is running on port", port);
     
