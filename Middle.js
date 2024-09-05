4dd253d8b0b7811c6b4167e99a13f7e4e39611c6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-05 11:34:28 +0530

    Basic server with status 200

--- Middle.js
+++
@@ -1,9 +0,0 @@
-module.exports = ((req, res, next)=>{
-    if(req.query.login){
-        console.log("I have query for middleware ");
-        next();
-    }
-    else{
-        res.send("I don't have middleware");
-    }
-})
--- server.js
+++ server.js
@@ -1,32 +1,10 @@
 const express = require("express");
-const middle = require('./Middle');
 const app = express();
-const router = express.Router();
 const port = 5000;
 
-//Direct middle ware
-app.get("/", middle, (req, res)=>{
-    res.send("<h1>Hi I am Home page");
+app.get("/", (req, res)=>{
+    res.status(200).send("All ok")
 })
 
-//middleware for multiple route
-router.use(middle);
-
-router.get("/about", (req, res)=>{
-    res.send("<h1>I am About page</h1>")
-})
-
-router.get("/contact", (req, res)=>{
-    res.send("<h1>I am contact page</h1>")
-})
-
-app.get("/profile", (req, res)=>{
-    res.send("<h1>I am profile page</h1>");
-})
-
-//Route level Middleware
-app.use("/", router);
-app.listen(port, ()=>{
-    console.log("Application is running on port", port);
-    
-})
+app.listen(port, ()=>console.log("Application is live")
+);
